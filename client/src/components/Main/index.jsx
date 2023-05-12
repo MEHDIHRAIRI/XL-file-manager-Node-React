f63e5a84e4aa49a5756d19098f37dde09ec6e1a3
Hrairi Mehdi <49190228+MEHDIHRAIRI@users.noreply.github.com>
2023-05-12 15:36:23 +0200

    set username in navBar

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,17 +1,23 @@
+import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
 const Main = () => {
+  const [user, setUser] = useState({});
   const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
-
     window.location.reload();
   };
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("user")));
+  }, []);
 
   return (
     <div className={styles.main_container}>
       <nav className={styles.navbar}>
-        <h1>fakebook</h1>
+        <h1>
+          {user.firstName} {user.lastName}
+        </h1>
         <button className={styles.white_btn} onClick={handleLogout}>
           Logout
         </button>
