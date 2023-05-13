086b714ae91b81a84c77089f61bef1c1ae5c26b5
Hrairi Mehdi <49190228+MEHDIHRAIRI@users.noreply.github.com>
2023-05-13 05:41:51 +0200

    fix date parsing issue from xlsx

--- server/controller/fileManager.js
+++ server/controller/fileManager.js
@@ -2,7 +2,67 @@ const { linkFileToUser } = require("../service/fileManager");
 
 const linkFile = async (req, res) => {
   const file = await linkFileToUser(req.userId, req.body.fileName);
+  file.fileData.map((ligne) => {
+    validateCIN(ligne);
+    validateRNE(ligne);
+    validateREF(ligne);
+    console.log(Date.parse(ligne.Date_de_naissance));
+    if (ligne.Categorie == null || ligne.Categorie == 0) {
+      ligne.validCategorie = false;
+    } else {
+      ligne.validCategorie = true;
+    }
+    if (ligne.Qte == null || ligne.Qte == 0) {
+      ligne.validQte = false;
+    } else {
+      ligne.validQte = true;
+    }
+    if (ligne.Institutionnel !== "Non" && ligne.Institutionnel !== "Oui") {
+      ligne.validInstitutionnel = false;
+    } else {
+      ligne.validInstitutionnel = true;
+    }
+  });
+  console.log(file.fileData);
   res.status(200).json({ file });
+};
+
+const validateRNE = (ligne) => {
+  if (
+    ligne.Nom.search("Sté") == false &&
+    (ligne.RNE == null || ligne.RNE == "")
+  ) {
+    ligne.validRNE = false;
+  } else {
+    ligne.validRNE = true;
+  }
+  return ligne;
+};
+
+const validateREF = (ligne) => {
+  if (ligne.Categorie !== null && ligne.Categorie !== undefined) {
+    if (
+      (!ligne.Categorie.search("OPF") ||
+        !ligne.Categorie.search("PG") ||
+        !ligne.Categorie.search("étrangé") ||
+        !ligne.Categorie.search("Etrange")) &&
+      (ligne.Ref === 0 || typeof ligne.Ref !== "number" || ligne.Ref == null)
+    ) {
+      ligne.validRef = false;
+    } else {
+      ligne.validRef = true;
+    }
+  }
+  return ligne;
+};
+
+const validateCIN = (ligne) => {
+  if (ligne.CIN.toString().length !== 8) {
+    ligne.validCIN = false;
+  } else {
+    ligne.validCIN = true;
+  }
+  return ligne;
 };
 
 module.exports = { linkFile };
--- server/service/fileManager.js
+++ server/service/fileManager.js
@@ -1,14 +1,28 @@
 const { User } = require("../models/user");
+const xlsx = require("xlsx");
+const path = require("path");
 
 const linkFileToUser = async (userId, fileName) => {
-  const filePath = `../../uploads/${fileName}`;
   const file = {
     fileName: fileName,
-    filePath: filePath,
+    filePath: path.join(__dirname, "..", "..", "uploads/", fileName),
   };
-
+  const fileData = await openAndManageFile(file.filePath);
   const updatedFileName = await User.findByIdAndUpdate(userId, { file: file });
   if (!updatedFileName) throw BadRequestError;
+  return { updatedFileName, fileData };
+};
+
+const openAndManageFile = async (filePath) => {
+  const workbook = xlsx.readFile(filePath, { cellDates: true });
+  const sheet_name_list = workbook.SheetNames;
+
+  if (sheet_name_list.length > 1) {
+    throw new Error("Multiple sheets are not allowed");
+  }
+
+  const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+  return data;
 };
 
 module.exports = { linkFileToUser };
